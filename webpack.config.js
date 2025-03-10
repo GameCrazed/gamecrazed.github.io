07b7743fc3a71bd28dc06164347f774150934218
Callum Eidson <callum.eidson@hotmail.co.uk>
2025-03-10 10:57:42 +0000

    Using the Copy Webpack Plugin to ensure the .sqlite3 files are included when deployed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -47,6 +48,11 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'MandMDataStore.sqlite3', to: 'MandMDataStore.sqlite3' },
+      ],
     }),
   ],
 };
